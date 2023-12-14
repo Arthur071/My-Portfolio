f91367678f7527ff0ea08267314e4fb92f8931d3
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-15 01:18:42 +0530

    data

---
+++ assets/js/data.js
@@ -0,0 +1,18 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['Front-end development is the practice of building the user interface (UI) and user experience (UX) of web applications and websites.It is the part of web development that users see and interact with directly.', 'Front-end developers use a variety of technologies, including HTML, CSS, and JavaScript, to create the look and feel of a website. They may also use other languages and frameworks, such as React, Angular, and Vue.js, to create more complex and interactive web applications.', 'Front-end development is a rapidly evolving field. New technologies and frameworks are emerging all the time, so it is important for front-end developers to stay up-to-date with the latest trends. There are many resources available for learning front-end development, including online courses, tutorials, and books.']
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: ['Backend development refers to the server-side logic and infrastructure that supports a website or application. Its the backbone that processes data, handles user interactions, and ensures everything runs smoothly and securely. Think of it as the brain behind the muscles of the front-end, dictating what happens and how.', 'While you interact with the sleek interfaces and animations of a website or app, a hidden world hums away, powering its functionality and making it tick. This is the domain of backend development, the often unseen magic that brings your digital experience to life.', 'Backend developers wield languages like Python, Java, Node.js, and PHP to build and maintain the server-side code that powers the application.']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['Full-stack development refers to the mastery of both front-end and back-end development skills. Its about understanding the entire digital ecosystem, from the users clicks and swipes on the screen to the complex algorithms and databases humming away in the server room.', 'Imagine a website or app as a magnificent bridge, its elegant arches spanning the gap between the users world and the vast data ocean. Full-stack development is the art and science of building this bridge, meticulously crafting both the visible superstructure and the unseen foundations that hold it all together.', 'Full-stack developers are in immense demand across all industries, leading to higher salaries and job security.']
+    },
+]
+
