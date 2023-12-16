a573a3818120dc7bbeceae27ef8d686d697e33ab
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-16 19:45:35 +0530

    img

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,3 +24,72 @@ servicesButtons.forEach(item => {
         item.classList.add('active')
     })
 })
+
+
+// Mixitup 
+const containerEl = document.querySelector('.projects--container');
+var mixer = mixitup(containerEl, {
+    animation: {
+        enable: false
+    }
+
+});
+
+mixer.filter('*');
+
+
+// swiper js
+
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
+
+
+
+
+const navMenu = document.querySelector('.nav--menu')
+const navOpenBtn = document.querySelector('.nav-toggle-open')
+const navCloseBtn = document.querySelector('.nav-toggle-close')
+const openNavHandler = () => {
+    navMenu.style.display = 'flex';
+    navOpenBtn.style.display = 'none';
+    navCloseBtn.style.display = 'inline-block';
+}
+const closeNavHandler = () => {
+    navMenu.style.display = 'none';
+    navOpenBtn.style.display = 'inline-block';
+    navCloseBtn.style.display = 'none';
+}
+navOpenBtn.addEventListener('click', openNavHandler)
+navCloseBtn.addEventListener('click', closeNavHandler)
+
+
+const navItems = navMenu.querySelectorAll('a');
+if (window.innerWidth < 768) {
+    navItems.forEach (item => {
+        item.addEventListener('click', closeNavHandler)
+    })
+}
+
+
+AOS.init({
+    offset: 80,
+    delay: 150,
+    duration: 1500,
+    once: false,
+    mirror: true,
+});
