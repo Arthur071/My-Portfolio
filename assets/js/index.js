e932694064451e8acaadf6936aed8d84f9f48b60
Shadan ijmal <shadanijmal999@gmail.com>
2023-12-17 22:57:45 +0530

    main js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,95 +1,86 @@
-const servicesButtons = document.querySelectorAll('.service--item');
-const serviceDetails = document.querySelector('.services--right');
-
+const servicesButtons = document.querySelectorAll(".service--item");
+const serviceDetails = document.querySelector(".services--right");
 
 const getService = (category) => {
-    const details = servicesData.find(item => item.category === category);
-    serviceDetails.innerHTML = `
+  const details = servicesData.find((item) => item.category === category);
+  serviceDetails.innerHTML = `
         <h3>${details.title}</h3>
-    ${details.description.map(paragraph => "<p>" + paragraph + "</p>").join('')}
-    `
-}
+    ${details.description
+      .map((paragraph) => "<p>" + paragraph + "</p>")
+      .join("")}
+    `;
+};
 const removeActiveClass = () => {
-    servicesButtons.forEach(button => {
-        button.classList.remove('active');
-    })
-}
+  servicesButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+};
 
-
-servicesButtons.forEach(item => {
-    item.addEventListener('click', () => {
-        removeActiveClass();
-        const serviceClass = item.classList[1];
-        getService(serviceClass)
-        item.classList.add('active')
-    })
-})
-
-
-// Mixitup 
-const containerEl = document.querySelector('.projects--container');
-var mixer = mixitup(containerEl, {
-    animation: {
-        enable: false
-    }
-
+servicesButtons.forEach((item) => {
+  item.addEventListener("click", () => {
+    removeActiveClass();
+    const serviceClass = item.classList[1];
+    getService(serviceClass);
+    item.classList.add("active");
+  });
 });
 
-mixer.filter('*');
+// Mixitup
+const containerEl = document.querySelector(".projects--container");
+var mixer = mixitup(containerEl, {
+  animation: {
+    enable: false,
+  },
+});
 
+mixer.filter("*");
 
 // swiper js
 
-
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        600: {
-            slidesPerView: 2
-        },
-        1024: {
-            slidesPerView: 3
-        }
-    }
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
 
-
-
-
-const navMenu = document.querySelector('.nav--menu')
-const navOpenBtn = document.querySelector('.nav-toggle-open')
-const navCloseBtn = document.querySelector('.nav-toggle-close')
+const navMenu = document.querySelector(".nav--menu");
+const navOpenBtn = document.querySelector(".nav-toggle-open");
+const navCloseBtn = document.querySelector(".nav-toggle-close");
 const openNavHandler = () => {
-    navMenu.style.display = 'flex';
-    navOpenBtn.style.display = 'none';
-    navCloseBtn.style.display = 'inline-block';
-}
+  navMenu.style.display = "flex";
+  navOpenBtn.style.display = "none";
+  navCloseBtn.style.display = "inline-block";
+};
 const closeNavHandler = () => {
-    navMenu.style.display = 'none';
-    navOpenBtn.style.display = 'inline-block';
-    navCloseBtn.style.display = 'none';
-}
-navOpenBtn.addEventListener('click', openNavHandler)
-navCloseBtn.addEventListener('click', closeNavHandler)
+  navMenu.style.display = "none";
+  navOpenBtn.style.display = "inline-block";
+  navCloseBtn.style.display = "none";
+};
+navOpenBtn.addEventListener("click", openNavHandler);
+navCloseBtn.addEventListener("click", closeNavHandler);
 
-
-const navItems = navMenu.querySelectorAll('a');
+const navItems = navMenu.querySelectorAll("a");
 if (window.innerWidth < 768) {
-    navItems.forEach(item => {
-        item.addEventListener('click', closeNavHandler)
-    })
+  navItems.forEach((item) => {
+    item.addEventListener("click", closeNavHandler);
+  });
 }
-
 
 AOS.init({
-    offset: 80,
-    delay: 150,
-    duration: 1500,
-    once: false,
-    mirror: true,
+  offset: 80,
+  delay: 150,
+  duration: 1500,
+  once: false,
+  mirror: true,
 });
